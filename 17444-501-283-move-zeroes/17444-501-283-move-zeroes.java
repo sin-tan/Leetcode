07f0d731d8a843ac94e67bb4f428dfa8762d6765
Tanishka Singh <112722128+sin-tan@users.noreply.github.com>
2025-01-30 08:42:03 +0530

    Time: 2 ms, Memory: 45.5 MB - LeetHub

---
+++ 17444-501-283-move-zeroes/17444-501-283-move-zeroes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int j=-1;
+         //find the first non zero element
+         for(int i=0;i<nums.length;i++)
+         {
+            if(nums[i]==0){
+           j=i;
+           break;}
+         }
+         if(j==-1)
+         return ;
+ for(int i=j+1;i<nums.length;i++)
+ {
+ if(nums[i]!=0)
+ {
+    int temp=nums[i];
+    nums[i]=nums[j];
+    nums[j]=temp;
+    j++;
+    }
+ 
+
+ }
+ 
+         
+      
+       }
+}
